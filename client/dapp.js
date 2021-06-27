38824fc2a7bd8f5ce3cb3068677e2284fb8fe052
Stefano Fedeli <stefano5.fedeli@mail.polimi.it>
2021-06-27 16:17:25 +0200

    Working Booking Dapp

--- client/dapp.js
+++ client/dapp.js
@@ -2,7 +2,7 @@
 // https://stackoverflow.com/questions/53936076/problems-with-metamask-web3-connection
 
 
-var ABI = [
+var ABIChainLink = [
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
@@ -125,27 +125,513 @@ var ABI = [
 	}
 ];
 
+var ABIBookmaker = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "balance",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint8",
+				"name": "quantity",
+				"type": "uint8"
+			}
+		],
+		"name": "buy",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "cancel_offer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_team",
+				"type": "string"
+			}
+		],
+		"name": "collect_bet",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "collect_dust",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_team",
+				"type": "string"
+			}
+		],
+		"name": "getAvailableTokensOnTeam",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "_team",
+				"type": "string"
+			}
+		],
+		"name": "getNumberTokensOnTeam",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "getOfferInfo",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getPricePerUnit",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getTeamNames",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "last_offer_id",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "offer_getOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "own",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "offer_isActive",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "active",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_team",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "place_bet",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "pair",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "sell",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "time_end_tournament",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "time_start_playoff",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
 
 var dapp;
-if (window.ethereum) {
-	window.ethereum.send('eth_requestAccounts');
-	dapp = new Web3(window.ethereum);
-} else {
-	document.getElementById('meta-mask-required').innerHTML = 'You need <a href="https://metamask.io/">MetaMask</a> browser plugin to run this example'
-}
+var bookmaker;
+var myAddr;
+var teams = ["ITA","ENG","WAL"]
+
+
+
+
+/*
+var LinkContract = new dapp.eth.Contract(ABI,"0x28CA76a5c0a50D3fbd8999b1336Eb9d01677f699");
+
 
 dapp.eth.defaultAccount = dapp.eth.accounts[0];
 
-var LinkContract = new dapp.eth.Contract(ABI,"0x0059b92830614d052384916bd7E0532912650649");
+
 LinkContract.defaultChain = "kovan"
 LinkContract.defaultAccount = "0x5D4b3E0B0faAdE843021e512E534c868387B46F9"
 
-console.log(LinkContract)
+//console.log(LinkContract.methods.res)
 
-//LinkContract.methods.isTeamOutAtStage("ITA").send({from:"0x5D4b3E0B0faAdE843021e512E534c868387B46F9"})
+LinkContract.methods.isTeamOutAtStage("ITA").send({from:"0x5D4b3E0B0faAdE843021e512E534c868387B46F9"})
+//LinkContract.methods.withdrawLink().send({from:"0x5D4b3E0B0faAdE843021e512E534c868387B46F9"})
 
-LinkContract.methods.getTeamInfo('ITA').call().then(function(result){
+LinkContract.methods.getTeamInfo("ITA").call().then(function(result){
 	console.log('FUNCTION RES: ')
     console.log(result)
 });
+*/
 //console.log(dapp.eth.getAccounts())
+
+
+window.onload = function funLoad() { 
+	if (window.ethereum) {
+		window.ethereum.send('eth_requestAccounts');
+		dapp = new Web3(window.ethereum);
+	} else {
+		document.getElementById('meta-mask-required').innerHTML = 'You need <a href="https://metamask.io/">MetaMask</a> browser plugin to run this example'
+	}
+
+	bookmaker = new dapp.eth.Contract(ABIBookmaker,"0x683fa5AbD089c3f278DBA5604d5E74D68211aEab");
+
+	dapp.eth.getAccounts().then( function(result) {
+		myAddr = result[0]
+		document.getElementById('eth.addr').textContent = myAddr;
+		teams.forEach( el => {
+			bookmaker.methods.getNumberTokensOnTeam(myAddr,el).call().then(function(res){
+				if (res > 0) {
+					document.getElementById('eth.account').innerHTML += ("<div class='"+ el +" bet-icon'></div>")
+				}
+			})
+		})
+		dapp.eth.getBalance(myAddr).then( function(res) {
+			document.getElementById('eth.eth').textContent = dapp.utils.fromWei(res, 'ether');
+		})
+	})
+	
+} 
+
+
+
+function bet() {
+	var teamtoBet = document.getElementById('teamToBetOn').value;
+	var qty = document.getElementById('qty_bet').value;
+	var ether = (qty*0.003).toString();
+	bookmaker.methods.place_bet(teamtoBet,qty).send({from: myAddr, value: dapp.utils.toWei(ether, 'ether')}).then(function(success){
+		if (success) {
+			alert("YOU HAVE PLACED YOUR BET");
+			window.location.reload()
+		} else {
+			alert("ERROR IN THE TRANSACTION");
+		}
+	})
+}
+
+function cashback() {
+	var team = document.getElementById('teamToCash').value;
+	bookmaker.methods.collect_bet(team).send({from: myAddr}).then(function(success){
+		if (success) {
+			alert("YOU HAVE PLACED YOUR BET");
+			window.location.reload()
+		} else {
+			alert("ERROR IN THE TRANSACTION");
+		}
+	})
+}
